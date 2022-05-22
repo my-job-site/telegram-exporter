30e2d66162b90958105ee20ddf0f6eda8a1f4689
Kirill Mil <kirill.mil.0310@mail.ru>
2022-05-22 12:55:26 +0700

    прикрутил телегу

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import config as cfg
 
 from flask import Flask, jsonify, request
@@ -17,11 +19,11 @@ def send_in_tg_chat():
     city = json.get('city', 'no data')
     tags = ', '.join(json.get('tags', ['no data']))
 
-    bot.send_message(
+    asyncio.run(bot.send_message(
         cfg.CHAT_ID,
         cfg.SAMPLE_TEXT.format(name=name, tags=tags, description=description, price=price, city=city),
-    )
-    return jsonify({'status: ok'}), 200
+    ))
+    return jsonify({'status': 'ok'}), 200
 
 
 if __name__ == "__main__":
