848e6412b39357d9075fbdc26f19606a89fe64d8
Kirill Mil <kirill.mil.0310@mail.ru>
2022-05-22 13:05:19 +0700

    поддержка html

--- src/main.py
+++ src/main.py
@@ -22,7 +22,7 @@ def send_in_tg_chat():
     asyncio.run(bot.send_message(
         cfg.CHAT_ID,
         cfg.SAMPLE_TEXT.format(name=name, tags=tags, description=description, price=price, city=city),
-        parse_mode='HTML'
+        parse_mode='html'
     ))
     return jsonify({'status': 'ok'}), 200
 
