e82a721cd8a8de1a2b59c5d90e27427d4f85ef8f
Kirill Mil <kirill.mil.0310@mail.ru>
2022-05-22 12:23:14 +0700

    flask server

---
+++ src/main.py
@@ -0,0 +1,28 @@
+import config as cfg
+
+from flask import Flask, jsonify, request
+from aiogram import Bot
+
+app = Flask(__name__)
+bot = Bot(token=cfg.TELEGRAM_TOKEN)
+
+
+@app.route("/send-to-telegram", methods=["POST"])
+def send_in_tg_chat():
+    json = request.get_json()
+
+    name = json.get('name', 'no data')
+    description = json.get('description', 'no data')
+    price = json.get('price', 'no data')
+    city = json.get('city', 'no data')
+    tags = ', '.join(json.get('tags', ['no data']))
+
+    bot.send_message(
+        cfg.CHAT_ID,
+        cfg.SAMPLE_TEXT.format(name=name, tags=tags, description=description, price=price, city=city),
+    )
+    return jsonify({'status: ok'}), 200
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=5000, debug=True)
